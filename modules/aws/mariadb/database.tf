5d3a7b5c944fabcf838713e48fe4651e37920adf
martivo <martin.vool@entigo.com>
2024-10-29 14:33:03 +0200

    Add DB

---
+++ modules/aws/mariadb/database.tf
@@ -0,0 +1,66 @@
+
+resource "random_password" "database" {
+  length = 20
+  special = false
+}
+
+resource "aws_security_group" "database" {
+  name        = var.prefix
+  description = var.prefix
+  vpc_id      = var.vpc_id
+  tags = {
+    Name = "Allow kubernetes access to database"
+  }
+}
+
+resource "aws_security_group_rule" "database-i" {
+  type              = "ingress"
+  from_port   = 3306
+  to_port     = 3306
+  protocol    = "tcp"
+  cidr_blocks = var.allowed_subnets
+  security_group_id = aws_security_group.database.id
+}
+
+resource "aws_db_instance" "database" {
+  allocated_storage     = var.allocated_storage
+  max_allocated_storage = var.max_allocated_storage
+  storage_type         = "gp3"
+  db_subnet_group_name = var.database_subnet_group
+  engine               = "mariadb"
+  engine_version       = "10.11.9"
+  instance_class       = var.instance_class
+  db_name                 = var.prefix
+  identifier           = var.prefix
+  username             = "rdsroot"
+  password             = random_password.database.result
+  auto_minor_version_upgrade = true
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_retention_period = 14
+  multi_az             = false
+  apply_immediately    = true
+  publicly_accessible = false
+  skip_final_snapshot = true
+  deletion_protection = false
+  vpc_security_group_ids = []
+  lifecycle {
+      ignore_changes = [ engine_version ]
+  }
+}
+
+resource "aws_secretsmanager_secret" "database" {
+  name = "${var.prefix}-database"
+}
+
+
+resource "aws_secretsmanager_secret_version" "database" {
+  secret_id     = aws_secretsmanager_secret.database.id
+  secret_string = jsonencode({
+    hostname              = aws_db_instance.database.address
+    password          = random_password.database.result
+    username              = "rdsroot"
+    dbname                = var.prefix
+  })
+}
+
+
---
+++ modules/aws/mariadb/variables.tf
@@ -0,0 +1,30 @@
+variable "prefix" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "allowed_subnets" {
+  type = list(string)
+}
+
+variable "database_subnet_group" {
+  type = string
+}
+
+variable "instance_class" {
+  type = string
+  default = "db.t3.small"
+}
+
+variable "allocated_storage" {
+  type = number
+  default = 20
+}
+
+variable "max_allocated_storage" {
+  type = number
+  default = 40
+}
