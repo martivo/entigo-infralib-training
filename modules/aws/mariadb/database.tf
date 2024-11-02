662919ac5b73b47931cfaa0b7999aad15f4deda1
martivo <martin.vool@entigo.com>
2024-11-02 07:36:03 +0200

    Change backup setings and maintenance window

--- modules/aws/mariadb/database.tf
+++ modules/aws/mariadb/database.tf
@@ -35,8 +35,8 @@ resource "aws_db_instance" "database" {
   username             = "rdsroot"
   password             = random_password.database.result
   auto_minor_version_upgrade = false
-  maintenance_window = "Mon:00:00-Mon:03:00"
-  backup_retention_period = 0
+  maintenance_window = "Sun:00:00-Sun:03:00"
+  backup_retention_period = 7
   monitoring_interval = 0
   multi_az             = false
   apply_immediately    = true
